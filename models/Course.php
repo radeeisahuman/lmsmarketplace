88ddcd90ec6a35a2484bae0d4ab23fbc4d8bdf3a
Radee <radeehasadream@gmail.com>
2025-04-14 13:10:17 +0600

    Decorator Pattern Added - For Course Lessons, Quizzes, Assignments

--- models/Course.php
+++ models/Course.php
@@ -1,7 +1,54 @@
 <?php
 
+// Factory and Decorator
+
 interface Course{
     public function create(): void;
+}
+
+class CourseAddOns implements Course{
+    protected Course $course;
+
+    public function __construct(Course $course){
+        $this->course = $course;
+    }
+
+    public function create(): void{
+        $this->course->create();
+    }
+}
+
+class Lesson extends CourseAddOns{
+    public function create(): void{
+        $this->course->create();
+        $this->addLesson();
+    }
+
+    public function addLesson(){
+        echo "You added a Lesson";
+    }
+}
+
+class Quiz extends CourseAddOns{
+    public function create(): void{
+        $this->course->create();
+        $this->addQuiz();
+    }
+
+    public function addQuiz(){
+        echo "You added a Quiz";
+    }
+}
+
+class Assignment extends CourseAddOns{
+    public function create(): void{
+        $this->course->create();
+        $this->addAssignment();
+    }
+
+    public function addAssignment(){
+        echo "You added a Assignment";
+    }
 }
 
 class TextCourse implements Course{
